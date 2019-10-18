514eff511b6f0b75348379dd6f6a66aec4c8e860
Mimi Hoang <miminhoang1@gmail.com>
2019-10-18 11:43:09 -0400

    Users

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,58 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
+const Users = require('../users/user-model.js');
+
+// for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      // add info about our user to the session
+      req.session.user = saved;
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
+  });
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // add info about our user to the session
+        req.session.user = user;
+        res.status(200).json({
+          message: `Welcome ${user.username}, You are logged in`,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+// router.get('/logout', (req, res) => {
+//   if (req.session) {
+//     req.session.destroy(err => {
+//       if (err) {
+//         res.json({
+//           message: "you can checkout but you can't leave"
+//         });
+//       } else {
+//         res.end();
+//       }
+//     })
+//   }
+// });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,14 @@
   before granting access to the next middleware/route handler
 */
 
+const bcrypt = require('bcryptjs');
+const Users = require('../users/user-model.js');
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+ 
+  console.log('req session', req.session);
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ message: 'You Shall not Pass!' });
+  }
 };
